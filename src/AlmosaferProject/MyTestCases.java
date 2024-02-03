df024fe87e3797a94937a3f2176152e2865c9313
AdnanHaija <adnanabulhaija@gmail.com>
2024-02-03 17:49:31 +0300

    update

--- src/AlmosaferProject/MyTestCases.java
+++ src/AlmosaferProject/MyTestCases.java
@@ -3,15 +3,17 @@ package AlmosaferProject;
 import static org.testng.Assert.assertEquals;
 
 import java.time.Duration;
+import java.time.LocalDate;
 import java.util.Random;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class MyTestCases extends Parameters{
+public class MyTestCases extends Parameters {
 
 	@BeforeTest
 	public void mySetup() {
@@ -30,7 +32,7 @@ public class MyTestCases extends Parameters{
 
 	}
 
-	@Test(priority = 1)
+	@Test(priority = 1, enabled = false)
 	public void CheckTheDeafultLanguageIsEnglish() {
 		String ExpectedLanaguage = "EN";
 		String ActualLanguage = driver.findElement(By.tagName("html")).getAttribute("lang").toUpperCase();
@@ -38,7 +40,7 @@ public class MyTestCases extends Parameters{
 
 	}
 
-	@Test(priority = 2)
+	@Test(priority = 2, enabled = false)
 	public void CheckTheDefaultCurrencyIsSAR() throws InterruptedException {
 		String ExpectedCurrency = "SAR";
 		String ActualCurrency = driver.findElement(By.xpath("//button[@data-testid='Header__CurrencySelector']"))
@@ -46,7 +48,7 @@ public class MyTestCases extends Parameters{
 		assertEquals(ActualCurrency, ExpectedCurrency);
 	}
 
-	@Test(priority = 3)
+	@Test(priority = 3, enabled = false)
 	public void CheckContactNumber() throws InterruptedException {
 		String ExpectedContactNumber = "+966554400000";
 		String ActualContactNumber = driver.findElement(By.tagName("strong")).getText();
@@ -54,7 +56,7 @@ public class MyTestCases extends Parameters{
 		assertEquals(ActualContactNumber, ExpectedContactNumber);
 	}
 
-	@Test(priority = 4)
+	@Test(priority = 4, enabled = false)
 	public void CheckQitafLogo() {
 		WebElement theFooter = driver.findElement(By.tagName("footer"));
 
@@ -63,13 +65,31 @@ public class MyTestCases extends Parameters{
 		assertEquals(theFooter.findElement(By.cssSelector(".sc-bdVaJa.bxRSiR.sc-ekulBa.eYboXF")).isDisplayed(), true);
 	}
 
-	@Test(priority = 5)
+	@Test(priority = 5, enabled = false)
 	public void CheckHotelTabIsNotSelectedByDefault() {
 
 		;
 		assertEquals(driver.findElement(By.id("uncontrolled-tab-example-tab-hotels")).getAttribute("aria-selected"),
 				"false");
 
+	}
+
+	@Test(priority = 6, enabled = false)
+	public void ChickDepartureDateAndRetunDate() {
+		LocalDate Today = LocalDate.now();
+		int ExpectedDepartureDate = Today.plusDays(1).getDayOfMonth();
+		int ExpectedReturnDate = Today.plusDays(2).getDayOfMonth();
+
+		int ActualDepartureDate = Integer.parseInt(driver
+				.findElement(By.cssSelector("div[class='sc-iHhHRJ sc-kqlzXE blwiEW'] span[class='sc-cPuPxo LiroG']"))
+				.getText());
+
+		int ActualReturnDate = Integer.parseInt(driver
+				.findElement(By.cssSelector("div[class='sc-iHhHRJ sc-OxbzP edzUwL'] span[class='sc-cPuPxo LiroG']"))
+				.getText());
+
+		assertEquals(ActualDepartureDate, ExpectedDepartureDate);
+		assertEquals(ActualReturnDate, ExpectedReturnDate);
 	}
 
 	@Test(priority = 7)
@@ -97,6 +117,59 @@ public class MyTestCases extends Parameters{
 
 	}
 
+	@Test(priority = 8)
+	public void SwitchToHotelTab() {
+		WebElement HotelTab = driver.findElement(By.id("uncontrolled-tab-example-tab-hotels"));
+		HotelTab.click();
+
+		if (driver.getCurrentUrl().contains("ar")) {
+			WebElement SearchInputField = driver
+					.findElement(By.cssSelector("input[placeholder='البحث عن فنادق أو وجهات']"));
+			SearchInputField.sendKeys(CitiesInArabic[RandomCitiesInArabic]);
+
+		} else {
+			WebElement SearchInputField = driver
+					.findElement(By.cssSelector("input[placeholder='Search for hotels or places']"));
+			SearchInputField.sendKeys(CitiesInEnglish[RandomCitiesInEnglish]);
+		}
+		WebElement List = driver.findElement(By.className("phbroq-4"));
+		List.findElements(By.tagName("li")).get(1).click();
+	}
+
+	@Test(priority = 9)
+	public void SelectRoomRandomly() {
+		WebElement Vistor = driver.findElement(By.tagName("select"));
+		Select Selctor = new Select(Vistor);
+		if (driver.getCurrentUrl().contains("ar")) {
+			Selctor.selectByVisibleText("1 غرفة، 1 بالغ، 0 أطفال");
+		} else {
+			Selctor.selectByVisibleText("1 Room, 1 Adult, 0 Children");
+		}
+		
+		driver.findElement(By.className("btwWVk")).click();
+	}
+
+	@Test (priority = 10)
+	public void PageLoaded () throws InterruptedException {
+		Thread.sleep(20000);
+		String SearchResult = driver.findElement(By.className("sc-kAKrxA")).getText();
+		if (driver.getCurrentUrl().contains("ar")) {
+			boolean ActualResult = SearchResult.contains("وجدنا");
+			assertEquals(ActualResult, true);
+		} else {
+			boolean ActualResult = SearchResult.contains("found");
+			assertEquals(ActualResult, true);
+		}
+			
+	}
+	
+	@Test (priority = 11)
+	public void PriceSort () {
+		driver.findElement(By.className("sc-csuNZv")).click();;
+		WebElement PriceValue = driver.findElement(By.cssSelector(".sc-htpNat.KtFsv.col-9"));
+		PriceValue.findElements(By.className("Price__Value"));
+	}
+	
 	@AfterTest
 	public void myPostTest() {
 	}
